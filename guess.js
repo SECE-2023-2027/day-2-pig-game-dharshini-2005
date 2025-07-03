579acd995c1d395893b9405b02a86daed8a8e266
dharshini-2005 <154994162+dharshini-2005@users.noreply.github.com>
2025-07-03 23:31:44 +0530

    Add files via upload

---
+++ guess.js
@@ -0,0 +1,77 @@
+// Generate random number between 1 and 20
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber); // For debugging purposes
+let score = 20;
+let highscore = 0;
+
+// DOM elements
+const guessInput = document.getElementById('guess-input');
+const checkBtn = document.getElementById('check-btn');
+const againBtn = document.getElementById('again-btn');
+const message = document.getElementById('message');
+const scoreDisplay = document.getElementById('score');
+const highscoreDisplay = document.getElementById('highscore');
+const questionMark = document.getElementById('question-mark');
+
+function displayMessage(msg) {
+  message.textContent = msg;
+}
+function againbutton(){
+     
+}
+
+// Check Button Click
+checkBtn.addEventListener('click', () => {
+  const guess = Number(guessInput.value);
+
+  if (!guess) {
+    displayMessage(' No number!');
+     document.body.style.backgroundColor ='red';
+  } else if (guess === secretNumber) {
+    displayMessage(' Correct Number!   you Won the game');
+    guessInput.style.display= 'none';
+    checkBtn.style.display = 'none';
+   
+    questionMark.textContent = secretNumber;
+    document.body.style.backgroundColor = '#60b347';
+
+    if (score > highscore) {
+      highscore = score;
+      highscoreDisplay.textContent = highscore;
+    }
+
+  } 
+//   else if(guess!=secretNumber){
+//       document.body.style.backgroundColor = 'red'
+//   }
+  
+  else {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too high!' : ' Too low!');
+      score--;
+      scoreDisplay.textContent = score;
+      
+    } else {
+      displayMessage(' You lost the game!');
+      scoreDisplay.textContent = 0;
+      document.getElementById('guess-input').style.display = 'none';
+      document.getElementById('check-btn').style.display = 'none';
+        document.body.style.backgroundColor = 'red'
+      
+
+    }
+  }
+});
+
+// Again Button Click
+againBtn.addEventListener('click', () => {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  scoreDisplay.textContent = score;
+  guessInput.value = '';
+  displayMessage('Start guessing...');
+  questionMark.textContent = '?';
+  document.body.style.backgroundColor = '';
+  guessInput.style.display= 'inline-block';
+    checkBtn.style.display = 'inline-block';
+});
---
+++ index.js
@@ -0,0 +1,14 @@
+const h=document.getElementsByTagName("h");
+console.log(h);
+
+//id selector
+const i=document.getElementById("hello");
+console.log(i);
+//class selector
+const c=document.getElementsByClassName(:class_name);
+console.log(c);
+//queryselector
+const d=document.querySelector("h1");
+console.log('d');
+//query selectorall
+const d=document.querySelectorAll("h1")
